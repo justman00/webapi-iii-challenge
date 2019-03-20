36521fca5d0d7e99a270d85f9a1b82ed2c24dab9
Vlad <vovaturcan4@gmail.com>
2019-03-20 19:19:44 +0200

    added helmet

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const userRouter = require("./user-route/user-route");
 const postRouter = require("./post-route/post-route");
+const helmet = require("helmet");
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+
 server.use("/api/users", userRouter);
 server.use("/api/posts", postRouter);
 
